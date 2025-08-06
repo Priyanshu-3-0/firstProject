56f56f57984d8848d21b012af3a5b3fcfb6e0b76
priyanshu <priyanshujatan30@gmail.com>
2025-08-06 12:11:21 +0530

    Secoundcommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,10 +10,10 @@ public class Main {
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
-
+            for (int j = 1; j <= 5; j++) {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+            System.out.println(i + j);
+        }}
     }
 }
